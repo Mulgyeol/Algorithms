a1b2998cc5bc8697393806f71db1686da0c74311
Mulgyeol Jung <mulgyeoljung@gmail.com>
2021-02-16 16:55:05 +0900

    [Add] acmicpc_2961_doyoungFood

--- acmicpc/DFS/acmicpc_2961_doyoungFood.java
+++ acmicpc/DFS/acmicpc_2961_doyoungFood.java
@@ -1,4 +1,4 @@
-package com.ssafy.inclass;
+package com.algo.acmicpc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class acmicpc_2961_doyoungFood {
 	
 	private static int N, min = Integer.MAX_VALUE;
-	private static int[][] gredients;
+	private static int[][] ingredients;
 
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -16,29 +16,27 @@ public class acmicpc_2961_doyoungFood {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		gredients = new int[N][2];
+		ingredients = new int[N][2];
 		
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			gredients[i][0] = Integer.parseInt(st.nextToken());
-			gredients[i][1] = Integer.parseInt(st.nextToken());
+			ingredients[i][0] = Integer.parseInt(st.nextToken());
+			ingredients[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
 		findCook(0, 0, 1, 0);
 		System.out.println(min);
 	}
 
-	private static void findCook(int idx, int cnt, int mul, int sum) {
+	private static void findCook(int idx, int cnt, int sour, int bitter) {
 		if(idx >= N) {
 			if (cnt == 0) return;
-			if (Math.abs(mul-sum) < min) {
-				min = (Math.abs(mul-sum));
-			}
+			min = Math.min(Math.abs(sour-bitter),min);
 			return;
 		}
 		
-		findCook(idx+1,cnt+1,mul*gredients[idx][0],sum+gredients[idx][1]);
-		findCook(idx+1,cnt,mul,sum);
+		findCook(idx+1,cnt+1,sour*ingredients[idx][0],bitter+ingredients[idx][1]);
+		findCook(idx+1,cnt,sour,bitter);
 	}
 	
 
