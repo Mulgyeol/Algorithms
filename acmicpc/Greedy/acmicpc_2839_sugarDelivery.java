86106a5485450ef74e023d4f80ef1e2f86962807
Mulgyeol Jung <mulgyeoljung@gmail.com>
2021-02-16 18:14:17 +0900

    [Update] acmicpc_2839_sugarDelivery

--- acmicpc/Greedy/acmicpc_2839_sugarDelivery.java
+++ acmicpc/Greedy/acmicpc_2839_sugarDelivery.java
@@ -1,4 +1,4 @@
-package com.algo.acmicpc;
+package com.ssafy.homework;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,15 +14,15 @@ public class acmicpc_2839_sugarDelivery {
 		}else {
 			int five = N/5;
 			int sum = 0;
+			int count = 0; //count를 for문 안에서 초기화 안해줘도 된다.
 			for(int i=five; i>=0; i--) {
-				int count = 0;
 				do {
-					count++;
 					sum = i*5 + count*3;
+					count++;
 				}while(sum < N);
 				if(sum == N){
 					if(i == -1)	i++;
-					answer = i+count;
+					answer = i+ (count-1);
 					break;
 				}
 			}
